a1ff1f1a1d6bb23052b8b00d04970f1ed9d16a2f
suhalesharma-jpg <suhalesharma34@gmail.com>
2026-04-30 11:20:36 +0530

    Update script.js

--- meseum/script.js
+++ meseum/script.js
@@ -21,10 +21,7 @@ document.body.appendChild(startBtn);
 let session = null;
 
 // Try multiple URLs
-const MODEL_URLS = [
-  "https://cdn.jsdelivr.net/gh/suhalesharma-jpg/arvr_landers@v1.0/my_model.onnx",
-  "https://github.com/suhalesharma-jpg/arvr_landers/releases/download/v1.0/my_model.onnx"
-];
+const MODEL_URL = "https://huggingface.co/suhalesharma34/arvr-model/resolve/main/my_model.onnx";
 
 // Load model with fallback
 async function loadModel() {
