fbdf86b3212336b4dbeab919955bf629e552499f
suhalesharma-jpg <suhalesharma34@gmail.com>
2026-04-30 09:32:23 +0530

    Create set1.js

---
+++ delhi_meseum/data/anthropology/set1.js
@@ -0,0 +1,48 @@
+export const artworks = [
+
+  {
+    name: "Ajilamo Mask",
+    desc: `ID: 65.429
+20th Century CE
+Arunachal Pradesh, India
+Size: L. 51.5 cm; Wd 25 cm
+
+This mask is worn by the Sherdukpen community during Ajilamo dance. The Lhamo tradition originated in Tibet and represents celestial figures. Ritual performances depict mythological stories through dance and music.`
+  },
+
+  {
+    name: "Marriage Litter (Rahi)",
+    desc: `ID: 64.1270
+Material: Wood, Cloth
+Jharkhand, India
+20th Century CE
+Size: H. 146 cm
+
+Ceremonial litter used in Santhal weddings. Crafted with carvings of humans and animals and used in ritual marriage processions.`
+  },
+
+  {
+    name: "Basket (Phen)",
+    desc: `ID: M01/41
+Material: Bamboo, Cane, Natural pigments
+Arunachal Pradesh, India
+19th Century CE
+Size: H. 47 cm; Dia: 40 cm
+
+A traditional basket used in rituals, consisting of lid, tray, and stand. Designed with bamboo weaving techniques and inspired by Buddhist ritual vessels.`
+  },
+  
+  {
+    name: "Perak",
+    desc: `ID: 86.227
+Material: Textile, Fur, Semi-precious stones, Silver
+Ladakh, India
+19th Century CE
+Size: L. 69.5 cm; Wd 40.5 cm
+
+A traditional Ladakhi headdress worn by married women. It symbolizes social status and resembles a cobra hood, representing protection and cultural identity.`
+  },
+
+
+  
+];
