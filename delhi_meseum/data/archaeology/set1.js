dd6f4a969fd79adc77fc595893fc7a4f4d1400e8
suhalesharma-jpg <suhalesharma34@gmail.com>
2026-04-30 09:33:56 +0530

    Create set1.js

---
+++ delhi_meseum/data/archaeology/set1.js
@@ -0,0 +1,64 @@
+// data.js
+
+export const artworks = [
+  {
+    name: "Bharata",
+    desc: `Bronze | Vijayanagar | 14th CE
+
+Bharata carries Rama’s sandals symbolizing devotion and rightful rule of Ayodhya.`
+  },
+  {
+    name: "Buddha Head",
+    desc: `Gupta Period | Sarnath
+
+Calm expression, half-closed eyes, symbolic curls and ushnisa.`
+  },
+  {
+    name: "Bhu-Devi",
+    desc: `Nayaka Period | 17th CE
+
+Earth goddess associated with Vishnu.`
+  },
+  {
+    name: "Flying Celestial",
+    desc: `Aihole | 7th CE
+
+Celestial couple floating in clouds.`
+  },
+  {
+    name: "Mohini",
+    desc: `Western Chalukya | 11th CE
+
+Graceful female figure holding a mirror.`
+  },
+  {
+    name: "Saraswati",
+    desc: `Marble | 12th CE
+
+Goddess of knowledge holding lotus, book, rosary and vase.`
+  },
+  {
+    name: "Buddha",
+    desc: `Gandhara | 2nd CE
+
+Standing Buddha with flowing robes.`
+  },
+  {
+    name: "Surya",
+    desc: `Konark | 13th CE
+
+Sun god with seven horses.`
+  },
+  {
+    name: "Vishnu",
+    desc: `Mathura | Gupta Period
+
+Standing Vishnu with ornaments and halo.`
+  },
+  {
+    name: "Ardhanarisvara",
+    desc: `Vijayanagar | 15th CE
+
+Half Shiva and Parvati form.`
+  }
+];
